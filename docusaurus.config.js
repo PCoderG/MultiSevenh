d0f7c92cd1b6a70a98757404a5790714e46f365a
MultiSeven <prathamguptawhjr540@gmail.com>
2023-02-27 07:29:52 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,8 +42,6 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/MultiSeven-HQ/MultiSeven-Site/edit/main/',
         },
         blog: {
           showReadingTime: true,
